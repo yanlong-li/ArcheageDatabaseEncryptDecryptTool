42d74ae3cb42d3048c9b48f806e837f252b20f84
Yanlongli <admin@yanlongli.com>
2018-11-15 09:44:29 +0800

    上古世纪数据库改变工具 Signed-off-by: Yanlongli <admin@yanlongli.com>

---
+++ Decrypt/Program.cs
@@ -0,0 +1,135 @@
+﻿
+using System.IO;
+
+using System.Security.Cryptography;
+
+
+namespace ConsoleApplication1
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Decrypt("compact.sqlite3", "decrypted.sqlite3", "decrypted.data");
+
+        }
+
+        public static void Decrypt(string encryptedFile, string decryptedFile, string keyFile)
+
+        {
+
+
+            byte[] key = new byte[16];
+
+            using (var stream = new FileStream(keyFile, FileMode.Open, FileAccess.Read))
+
+            {
+
+                stream.Read(key, 0, (int)stream.Length);
+
+            }
+
+            using (var memoryEncryptedStream = new MemoryStream())
+
+            using (var memoryDecryptedStream = new MemoryStream())
+
+            {
+
+                //read file with 16byte{ zero}
+
+                for (int i = 0; i < 16; i++)
+
+                {
+
+                    memoryEncryptedStream.WriteByte(0);
+
+                }
+
+                var temp = File.ReadAllBytes(encryptedFile);
+
+                memoryEncryptedStream.Write(temp, 0, temp.Length);
+
+
+
+
+                memoryEncryptedStream.Position = 0;
+
+
+                //decrypt
+
+                using (ICryptoTransform transform = new RijndaelManaged
+
+                {
+
+                    BlockSize = 128,
+
+                    IV = key,
+
+                    KeySize = 128,
+
+                    Key = key,
+
+                    Padding = PaddingMode.None,
+
+                    Mode = CipherMode.CBC
+
+                }.CreateDecryptor())
+
+                using (var cryptoStream = new CryptoStream(memoryDecryptedStream, transform, CryptoStreamMode.Write))
+
+                {
+
+
+                    byte[] buffer = new byte[1024];
+
+                    int len;
+
+                    while ((len = memoryEncryptedStream.Read(buffer, 0, buffer.Length)) > 0)
+
+                    {
+
+                        cryptoStream.Write(buffer, 0, len);
+                        //cryptoStream.FlushFinalBlock();
+
+                    }
+
+
+                    cryptoStream.Close();
+
+                    transform.Dispose();
+
+                }
+
+
+                //write file without head 16byte
+
+                using (var decryptedFileStream = new FileStream(decryptedFile, FileMode.Create, FileAccess.Write))
+
+                {
+
+                    var outTemp = memoryDecryptedStream.ToArray();
+
+                    decryptedFileStream.Write(outTemp, 16, outTemp.Length-16);
+
+                    decryptedFileStream.Close();
+
+                }
+
+
+                memoryEncryptedStream.Close();
+
+                memoryDecryptedStream.Close();
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Encrypt/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System.IO;
+using System.Security.Cryptography;
+namespace aadbencrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encrypt("decrypted.sqlite3", "compact.sqlite3", "decrypted.data", "head.sqlite3");
+        }
+        public static void Encrypt(string encryptedFile, string decryptedFile, string keyFile,string headFile)
+        {
+            byte[] key = new byte[16];
+            byte[] head = new byte[16];
+            using (var stream = new FileStream(keyFile, FileMode.Open, FileAccess.Read))
+            {
+                stream.Read(key, 0, (int)stream.Length);
+            }
+            using (var memoryEncryptedStream = new MemoryStream())
+            using (var memoryDecryptedStream = new MemoryStream())
+            {
+                using (var stream = new FileStream(headFile, FileMode.Open, FileAccess.Read))
+
+                {
+
+                    stream.Read(head, 0, (int)stream.Length);
+
+                }
+                memoryEncryptedStream.Write(head, 0, 16);
+                var temp = File.ReadAllBytes(encryptedFile);
+                memoryEncryptedStream.Write(temp, 0, temp.Length);
+
+
+                memoryEncryptedStream.Position = 0;
+
+                //decrypt
+                using (ICryptoTransform transform = new RijndaelManaged
+                {
+                    BlockSize = 128,
+                    IV = key,
+                    KeySize = 128,
+                    Key = key,
+                    Padding = PaddingMode.ANSIX923,
+                    Mode = CipherMode.CBC
+                }.CreateEncryptor())
+                using (var cryptoStream = new CryptoStream(memoryDecryptedStream, transform, CryptoStreamMode.Write))
+                {
+                    byte[] buffer = new byte[1024];
+                    int len;
+                    while ((len = memoryEncryptedStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        cryptoStream.Write(buffer, 0, len);
+                    }
+                    cryptoStream.Close();
+                    transform.Dispose();
+                }
+                //write file without head 16byte
+                using (var decryptedFileStream = new FileStream(decryptedFile, FileMode.Create, FileAccess.Write))
+                {
+                    var outTemp = memoryDecryptedStream.ToArray();
+                    decryptedFileStream.Write(outTemp, 16, outTemp.Length - 16);
+                    decryptedFileStream.Close();
+                }
+                memoryEncryptedStream.Close();
+                memoryDecryptedStream.Close();
+            }
+        }
+    }
+}
